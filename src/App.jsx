71875e78cd0c1308aec1b3cb316b53260862b72d
Alto0327 <aj.silvaf2003@gmail.com>
2025-09-30 08:54:00 -0400

    created Vans Page and server file

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,10 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import Home from './pages/Home/Home';
 import About from './pages/About/About';
+import Vans from './pages/Vans/Vans';
 import './App.css';
 
-
+import "./server"
 
 
 
@@ -24,6 +25,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
+        <Route path="/vans" element={<Vans />} />
       </Routes>
     </BrowserRouter>
   );
