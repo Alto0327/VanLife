17e242747990bb2149772c1abaabcc5835479a74
Alto0327 <aj.silvaf2003@gmail.com>
2025-09-30 09:12:38 -0400

    Connected the server to vans component and map over values

--- src/pages/Vans/Vans.jsx
+++ src/pages/Vans/Vans.jsx
@@ -1,3 +1,27 @@
+import React from "react";
+import { useEffect } from "react";
+
 export default function Vans(){
-    return <h1>Vans</h1>
+    const [vans, setVans] = React.useState([])
+    useEffect(() => {
+        fetch("/api/vans")
+            .then(res => res.json())
+            .then(data => setVans(data.vans))            
+    }
+    , [])
+    
+    return (
+        <>
+            {vans.map((van) =>(
+                <div>
+                    <h2>{van.name}</h2>
+                    <p>${van.price}</p>
+                    <img src={van.imageUrl} alt={van.name} />
+                    <p>{van.description}</p>
+                    <p>Type: {van.type}</p>
+                </div>
+            ))}
+        
+        </>
+    )
 }
