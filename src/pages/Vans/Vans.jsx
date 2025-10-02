b5c7ca3e4cc1b92ca6a44043defa95eb7eb4b20f
Alto0327 <aj.silvaf2003@gmail.com>
2025-10-02 08:34:47 -0400

    styling for vans page

--- src/pages/Vans/Vans.jsx
+++ src/pages/Vans/Vans.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { useEffect } from "react";
+import styles from './Vans.module.css'
 
 export default function Vans(){
     const [vans, setVans] = React.useState([])
@@ -12,16 +13,22 @@ export default function Vans(){
     
     return (
         <>
-            {vans.map((van) =>(
-                <div key={van.id}>
-                    <h2>{van.name}</h2>
-                    <p>${van.price}</p>
-                    <img src={van.imageUrl} alt={van.name} />
-                    <p>{van.description}</p>
-                    <p>Type: {van.type}</p>
-                </div>
-            ))}
+            <h1>Explore our van options</h1>
+            <div className={styles.vanContainer}>
+                {vans.map((van) =>(
+                    <div key={van.id} className={styles.van}>
+                        <img src={van.imageUrl} alt={van.name} className={styles.img}/>
+                        <div className={styles.vanInfo}>
+                            <h3 className={styles.title}>{van.name}</h3>
+                            <p className={styles.price}>${van.price}<span>/day</span></p>
+                        </div>
+                        <p className={styles.price}></p>
+                        <p className={styles.description}>{van.description}</p>
+                        <p className={`${styles.type} ${styles[van.type]}`}>{van.type}</p>
+                    </div>
+                ))}
         
+            </div>
         </>
     )
 }
