30f8baab5f0de73528b7c9557da01bc68d4d374a
Alto0327 <aj.silvaf2003@gmail.com>
2025-11-10 09:27:01 -0500

    Building host van details page

--- src/pages/Host/HostVanDetails.jsx
+++ src/pages/Host/HostVanDetails.jsx
@@ -1,3 +1,25 @@
+import { useParams } from "react-router-dom"
+import { useState, useEffect } from "react";
+
 export default function HostVansDetail(){
-    return <h1>Van detail</h1>
+    const {id} = useParams()
+
+    const [van, setVan] = useState(null);
+
+    useEffect(() => {
+        fetch(`/api/host/vans/${id}`)
+            .then(response => response.json())
+            .then(data => setVan(data.vans[0]))
+        },)
+
+    if (!van) return <h2>Loading...</h2>;
+
+    return (
+        <div>
+        <img src={van.imageUrl} />
+        <h1>{van.name}</h1>
+        <p>{van.type}</p>
+        <p>${van.price}/day</p>
+        </div>
+    );
 }
