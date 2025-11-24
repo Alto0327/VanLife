fdc886051bbbff9d6f8c574fca70ca14900b4810
Alto0327 <aj.silvaf2003@gmail.com>
2025-11-24 08:56:27 -0500

    refactor: clean Vans component, add type filtering, simplify fetch logic, and reorganize markup

--- src/pages/Vans/Vans.jsx
+++ src/pages/Vans/Vans.jsx
@@ -1,43 +1,44 @@
-import React from "react";
-import { useEffect } from "react";
-import styles from './Vans.module.css'
-import { Link } from "react-router-dom";
+import React from "react"
+import { Link, useSearchParams } from "react-router-dom"
+import styles from "./Vans.module.css"
 
-export default function Vans(){
+export default function Vans() {
+    const [searchParams] = useSearchParams()
     const [vans, setVans] = React.useState([])
-    const [loading, setLoading] = React.useState(true)
-    useEffect(() => {
+
+    const typeFilter = searchParams.get("type")
+
+    React.useEffect(() => {
         fetch("/api/vans")
             .then(res => res.json())
-            .then(data => setVans(data.vans)) 
-            console.log(vans)    
-            setLoading(false)       
-    }
-    , [])
-    
-    if (loading) {
-        return <h1 className={styles.loadingPlaceholder}>Loading vans...</h1> 
-    }
+            .then(data => setVans(data.vans))
+    }, [])
+
+    const displayedVans = typeFilter
+        ? vans.filter(van => van.type.toLowerCase() === typeFilter)
+        : vans
+
+    const vanElements = displayedVans.map(van => (
+        <div key={van.id} className={styles.van}>
+            <Link to={`/vans/${van.id}`}>
+                <img src={van.imageUrl} alt={van.name} className={styles.img} />
+                <div className={styles.vanInfo}>
+                    <h3 className={styles.title}>{van.name}</h3>
+                    <p className={styles.price}>${van.price}<span>/day</span></p>
+                </div>
+
+                <p className={`${styles.type} ${styles[van.type]}`}>
+                    {van.type}
+                </p>
+            </Link>
+        </div>
+    ))
 
     return (
         <>
             <h1>Explore our van options</h1>
             <div className={styles.vanContainer}>
-                {vans.map((van) =>(
-                    <Link to={"/vans/" + van.id}>
-                        <div key={van.id} className={styles.van}>
-                            <img src={van.imageUrl} alt={van.name} className={styles.img}/>
-                            <div className={styles.vanInfo}>
-                                <h3 className={styles.title}>{van.name}</h3>
-                                <p className={styles.price}>${van.price}<span>/day</span></p>
-                            </div>
-                            <p className={styles.price}></p>
-                            <p className={styles.description}>{van.description}</p>
-                            <p className={`${styles.type} ${styles[van.type]}`}>{van.type}</p>
-                        </div>
-                    </Link>
-                ))}
-        
+                {vanElements}
             </div>
         </>
     )
